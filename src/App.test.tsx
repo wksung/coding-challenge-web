448aa63c73da60e230ede70ef2c1dfc5e10fe2bf
wksung <michaelsung97@hotmail.com>
2024-11-28 14:38:08 +1000

    fixed testing and pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,1 +1,9 @@
-test('renders learn react link', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.queryByText(/learn react/i);
+    expect(linkElement).not.toBeInTheDocument();
+});
