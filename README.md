ff4919f9fb399ea62e840d058923b5f8476541de
wksung <michaelsung97@hotmail.com>
2024-11-28 18:18:42 +1000

    added env file checker

--- README.md
+++ README.md
@@ -1,25 +1,72 @@
-## Requirements:
+### Introduction
 
+This is a code challenge with specific requirements which are:
+ 
 1. Write some code to consume the json hosted on the above web service.
 2. Output a list of all the cats in alphabetical order under a heading of the gender of their owner.
 3. Output must be presentable on a web browser.
 4. Submissions will only be accepted via GitHub or Bitbucket.
 
-## Tech Stack:
+### Tech Stack
 
-- Frontend - React & TypeScript
-- CSS Framework - Material UI
-- Testing - RTL & Cypress
-- Tools - ESLint & Prettier
-- CI/CD - GitHub Actions & Netlify
+#### Client:
+React, Typescript
 
-## CI/CD:
+#### Styling:
+Material UI, CSS
 
-1. Push to main branch.
-2. GitHub Actions -> Runs ESLint & tests.
-3. Netlify -> Deploys site to link below.
-4. Netlify -> Make sure to publish if manual testing is completed.
+#### Testing:
+React Testing Library, Cypress
 
-## Live Site:
+#### Tools:
+Prettier, ESLint
 
-Link: https://code-challenge-web-29112024.netlify.app
+#### DevOps:
+Github Actions, Netlify
+
+### CI/CD Pipeline
+
+1. Whenever a code is pushed into the main branch the GitHub Actions workflow is triggered.
+2. Within the workflow, there are a couple of things the Actions check in order for the workflow to pass, these are:
+   - ESLint
+   - React Testing Library
+   - Cypress Component Testing
+3. While the workflow is simulataneously running, the public file on the folder is pushed and deployed to Netlify.
+4. This is deployed but not published which means tests should be checked as each deployment generates a unique URL before publishing the work.
+5. Once everything is ticked off, deployment can be published.
+
+### Run Locally
+
+Clone the project or download the zip
+
+```bash
+  git clone https://github.com/wksung/stock-dashboard.git
+```
+
+Go to the project directory
+
+```bash
+  cd stock-dashboard
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Change the .env.example file to .env and update the following
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `REACT_APP_API_URL` | `string` | **Required**. URL which was provided in the code challenge excluding the final / |
+
+Start the server
+
+```bash
+  npm start
+```
+
+### Live Site
+
+Or if you prefer, please visit [here](https://code-challenge-web-29112024.netlify.app) for the demo.
--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,15 @@ import Grid from '@mui/material/Grid2';
 const App = () => {
     const { owners, error, loading } = useFetchOwners();
 
+    const [envExist, setEnvExist] = useState<Boolean>(false);
     // split cats based on their owner's gender
     const [malePetOwners, setMaleOwners] = useState<Pet[]>([]);
     const [femaleOwners, setFemaleOwners] = useState<Pet[]>([]);
+
+    // check if ENV file exists
+    useEffect(() => {
+        if (process.env.REACT_APP_API_URL !== undefined) setEnvExist(true);
+    }, []);
 
     // retrieve cats name based on owner's gender & pet type
     useEffect(() => {
@@ -27,36 +33,45 @@ const App = () => {
             maxWidth="xl"
             sx={{ paddingTop: '2.5rem', paddingBottom: '2.5rem' }}
         >
-            <Grid container rowSpacing={1} spacing={2}>
-                <Grid size={12}>
-                    <h1 style={{ fontSize: '1rem' }}>Cat Names by Gender</h1>
+            {envExist ? (
+                <Grid container rowSpacing={1} spacing={2}>
+                    <Grid size={12}>
+                        <h1 style={{ fontSize: '1rem' }}>
+                            Cat Names by Gender
+                        </h1>
+                    </Grid>
+                    {loading ? (
+                        <Grid size={12}>
+                            <Feedback type="loading">Loading...</Feedback>
+                        </Grid>
+                    ) : error ? (
+                        <Grid size={12}>
+                            <Feedback type="error">{error}</Feedback>
+                        </Grid>
+                    ) : (
+                        <>
+                            <Grid size={6}>
+                                {' '}
+                                <PetCard
+                                    gender="Male"
+                                    pets={malePetOwners}
+                                ></PetCard>
+                            </Grid>
+                            <Grid size={6}>
+                                <PetCard
+                                    gender="Female"
+                                    pets={femaleOwners}
+                                ></PetCard>
+                            </Grid>
+                        </>
+                    )}
                 </Grid>
-                {loading ? (
-                    <Grid size={12}>
-                        <Feedback type="loading">Loading...</Feedback>
-                    </Grid>
-                ) : error ? (
-                    <Grid size={12}>
-                        <Feedback type="error">{error}</Feedback>
-                    </Grid>
-                ) : (
-                    <>
-                        <Grid size={6}>
-                            {' '}
-                            <PetCard
-                                gender="Male"
-                                pets={malePetOwners}
-                            ></PetCard>
-                        </Grid>
-                        <Grid size={6}>
-                            <PetCard
-                                gender="Female"
-                                pets={femaleOwners}
-                            ></PetCard>
-                        </Grid>
-                    </>
-                )}
-            </Grid>
+            ) : (
+                <Feedback type="error">
+                    Your environment file is missing, please follow the README
+                    instructions.
+                </Feedback>
+            )}
         </Container>
     );
 };
